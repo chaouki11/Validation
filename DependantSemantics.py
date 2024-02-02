929790bf768b8329470f2d5f461c5a3170d97d7b
youssef-berkia <youssefberkiaa@gmail.com>
2024-02-02 08:31:01 +0100

    started propriete et composition

---
+++ DependantSemantics.py
@@ -0,0 +1,33 @@
+
+
+import copy
+
+
+class DependantSemantics:
+    def initial(self):
+        pass
+
+    def actions(self, input,configuration):
+        pass
+    
+    def execute(self,action, input,configuration):
+        pass
+
+
+
+class DependantSoupSemantics(DependantSemantics):
+    def __init__(self,soup):
+        self.soup=soup
+
+    def initial(self):
+        return self.soup.initial()
+    
+    def actions(self, input,source):
+        return filter(lambda p:p.garde(input,source),self.soup.pieces())
+    
+    def execute(self,piece, input,source):
+        src=copy.deepcopy(source)
+        inp=copy.deepcopy(input)
+        return piece.action(inp,src)
+    
+
---
+++ StepSyncComposition.py
@@ -0,0 +1,45 @@
+
+from Semantic import Semantic
+ 
+class StepSynchComposition(Semantic):
+    def _init_(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def initial(self):
+        initials = []
+        for lhs_c in self.lhs.initial():
+            for rhs_c in self.rhs.initial():
+                c=lhs_c,rhs_c
+                initials.append(c)
+        return initials
+
+    def actions(self, source):
+        lhs_source,rhs_source=source
+        syncActions = []
+        lhs_actions = self.actions(lhs_source)
+        action_count = len(lhs_actions)
+        for lhs_action in lhs_actions:
+            lhs_targets = self.lhs.execute(lhs_action, lhs_source)
+            if len(lhs_targets)==0:
+                action_count-=1
+            for lhs_target in lhs_targets:
+                lhs_step=(lhs_source,lhs_action,lhs_target)
+                rhs_actions = self.rhs.actions(lhs_step, rhs_source)
+                actions=map(lambda rA: (lhs_step,rA),rhs_actions)
+                syncActions.extend(actions)    
+
+        if action_count==0:
+            lhs_step=lhs_source,"deadlock",lhs_source
+            rhs_actions=self.rhs.actions(lhs_step,lhs_source)
+            syncActions.extend(lambda rA: (lhs_step,rA),rhs_actions)
+
+        return syncActions
+
+    def execute(self,action, source):
+        lhs_step,rhs_action=action
+        lhs_source,rhs_source=source
+        rhs_targets=self.rhs.execute(rhs_action,lhs_step,rhs_source)
+
+
+        return map(lambda rhs_target:lhs_step[2],rhs_targets)
